a710561cc6d6887687c1fceeb83d343a240ff3df
franco-maidana <francomaidana094@gmail.com>
2023-06-13 19:21:43 -0300

    V 0.0.1
    comienzo interactivo de un supermercado

---
+++ script.js
@@ -0,0 +1,98 @@
+let nombreUsuario = prompt('Bienvenido, ingrese su nombre');
+
+while (nombreUsuario.trim() === '') {
+  alert('Nombre inválido, por favor ingrese el nombre nuevamente');
+  nombreUsuario = prompt('Por favor ingrese nuevamente su nombre');
+}
+
+let dineroDelUsuario = prompt('ingrese el dinero que piensa gastar');
+alert(`Bienvenido ${nombreUsuario}, comencemos a comprar`);
+
+
+const SeccionCategoria = Supermercado.map((producto) => {
+  return {
+    name: producto.categoria
+  };
+});
+
+const arregloPalabras = SeccionCategoria.map((categoria) => categoria.name);
+const palabrasFiltradas = [];
+
+let resultados = [];
+let productoAmostrar = [];
+let carritoDeCompras = [];
+
+// Obtenga las categorías únicas de la matriz Supermercado
+const categorias = [...new Set(Supermercado.map((producto) => producto.categoria))];
+
+let categoriaSeleccionada;
+if (categorias.length === 1) {
+  // Si solo hay una categoría, asígnala directamente a categoriaSeleccionada
+  categoriaSeleccionada = categorias[0];
+} else {
+  //   solicita al usuario que seleccione una categoria
+  categoriaSeleccionada = prompt(`Categorías disponibles:\n${categorias.join("\n")}\n\nIngrese la categoría a filtrar (o escriba 'finalizar' para terminar la compra):`);
+}
+
+while (categoriaSeleccionada.toLowerCase() !== "finalizar") {
+  const productosCategoria = Supermercado.filter(
+    (producto) => producto.categoria.toLowerCase() === categoriaSeleccionada.toLowerCase()
+  );
+
+  if (productosCategoria.length === 0) {
+    alert("No hay productos disponibles en la categoría seleccionada.");
+  } else {
+    productoAmostrar = []; // Restablecer la matriz para cada selección de categoría
+
+    productosCategoria.forEach((producto, index) => {
+      const nombre = producto.nombre;
+      const marca = producto.marca;
+      const precio = parseFloat(producto.precio.slice(1)); // Convierta el precio en un número sin el símbolo "$"
+      productoAmostrar.push(`${index + 1}. ${nombre} ${marca} $${precio}\n`);
+    });
+
+    let productosFinales = productoAmostrar.join('');
+    console.log(productosFinales);
+    let productoElegido = parseInt(prompt(`Productos disponibles:\n${productosFinales}\nIngrese el número del producto que desea agregar al carrito:`));
+
+    if (isNaN(productoElegido) || productoElegido < 1 || productoElegido > productosCategoria.length) {
+      alert("Por favor, ingrese un número válido correspondiente al producto.");
+    } else {
+      //Agregar el producto seleccionado al carrito de compras
+      carritoDeCompras.push(productosCategoria[productoElegido - 1]);
+      alert("Producto agregado al carrito de compras.");
+    }
+  }
+
+  categoriaSeleccionada = prompt(`Categorías disponibles:\n${categorias.join("\n")}\n\nIngrese la categoría a filtrar (o escriba 'finalizar' para terminar la compra):`);
+}
+
+let montoTotal = 0;
+let carritoFinal = [];
+
+// Calcule el monto total de la compra y crea una lista de productos en el carrito de compras.
+carritoDeCompras.forEach((producto) => {
+  const nombre = producto.nombre;
+  const marca = producto.marca;
+  const precio = parseFloat(producto.precio.slice(1)); //Convierta el precio en un número sin el símbolo "$"
+  montoTotal += precio;
+  carritoFinal.push(`${nombre} ${marca} $${precio}`);
+});
+
+console.log("Carrito de Compras:");
+console.table(carritoFinal);
+console.log(`Tu compra ha finalizado. El precio a pagar es: $ ${montoTotal} `);
+
+
+// Verificar si el dinero total es mayor que el monto de la compra si la compra supera el monto que ingreso no va a poder realizar la operacion.
+if (dineroDelUsuario >= montoTotal) {
+  alert("Puedes realizar la compra");
+  let vuelto = dineroDelUsuario - montoTotal;
+  alert(`Vuelto: $${vuelto}`);
+  console.table(`Vuelto: $${vuelto}`);
+} else {
+  alert("No tienes suficiente dinero para realizar la compra");
+  let debe = montoTotal - dineroDelUsuario;
+  alert(`Debe: $${debe}`);
+  console.table(`Debe: $${debe}`);
+}
---
+++ super.js
@@ -0,0 +1,156 @@
+let Supermercado = [
+  {
+    id: 1,
+    nombre: 'Aceite de girasol',
+    marca: 'Cañuelas',
+    precio: '$ 600',
+    categoria: 'Aceites',
+  },
+  {
+    id: 2,
+    nombre: 'Aceite de girasol',
+    marca: 'Cocinero',
+    precio: '$ 750',
+    categoria: 'Aceites'
+  },
+  {
+    id: 3,
+    nombre: 'Fideos mostachol',
+    marca: 'Arcor',
+    precio: '$  220',
+    categoria: 'Pastas'
+  },
+  {
+    id: 4,
+    nombre: 'Fideos mostachol',
+    marca: 'Matarazzo',
+    precio: '$ 235',
+    categoria: 'Pastas'
+  },
+  {
+    id: 5,
+    nombre: 'Fideos spaghetti',
+    marca: 'Arcor',
+    precio: '$ 220',
+    categoria: 'Pastas'
+  },
+  {
+    id: 6,
+    nombre: 'Fideos spaghetti',
+    marca: 'Matarazzo',
+    precio: '$ 240',
+    categoria: 'Pastas'
+  },
+  {
+    id: 7,
+    nombre: 'Fideos tirabuzon',
+    marca: 'Favorito',
+    precio: '$ 230',
+    categoria: 'Pastas'
+  },
+  {
+    id: 8,
+    nombre: 'Fideos tirabuzon',
+    marca: 'Terrabussi',
+    precio: '$ 220',
+    categoria: 'Pastas'
+  },
+  {
+    id: 9,
+    nombre: 'Harina integral',
+    marca: 'Pureza',
+    precio: '$ 260',
+    categoria: 'Harinas'
+  },
+  {
+    id: 10,
+    nombre: 'Harina de trigo',
+    marca: 'Caserita',
+    precio: '$ 190',
+    categoria: 'Harinas'
+  },
+  {
+    id: 11,
+    nombre: 'Harina integral',
+    marca: 'Cañuelas',
+    precio: '$ 225',
+    categoria: 'Harinas'
+  },
+  {
+    id: 12,
+    nombre: 'Harina de trigo',
+    marca: 'Pureza',
+    precio: '$ 240',
+    categoria: 'Harinas'
+  },
+  {
+    id: 13,
+    nombre: 'Lata de tomates',
+    marca: 'Arcor',
+    precio: '$ 175',
+    categoria: 'Salsas de tomate'
+  },
+  {
+    id: 14,
+    nombre: 'Lata de tomates',
+    marca: 'Salsati',
+    precio: '$ 220',
+    categoria: 'Salsas de tomate'
+  },
+  {
+    id: 15,
+    nombre: 'Lentejas ',
+    marca: 'Arcor',
+    precio: '$ 500',
+    categoria: 'Legumbres'
+  },
+  {
+    id: 16,
+    nombre: 'Lentejas ',
+    marca: 'La campagnola',
+    precio: '$ 440',
+    categoria: 'Legumbres'
+  },
+  {
+    id: 17,
+    nombre: 'Pure de tomates',
+    marca: 'Arcor',
+    precio: '$ 180',
+    categoria: 'Salsas de tomate'
+  },
+  {
+    id: 18,
+    nombre: 'Pure de tomates',
+    marca: 'Salsati',
+    precio: '$ 220',
+    categoria: 'Salsas de tomate'
+  },
+  {
+    id: 19,
+    nombre: 'Sal fina de mesa',
+    marca: 'Celusal',
+    precio: '$ 180',
+    categoria: 'Condimentos'
+  },
+  {
+    id: 20,
+    nombre: 'Sal fina de mesa',
+    marca: 'Maroleo',
+    precio: '$ 90',
+    categoria: 'Condimentos'
+  },
+  {
+    id: 21,
+    nombre: 'Vinegre de alcohol',
+    marca: 'Maroleo',
+    precio: '$ 240',
+    categoria: 'Condimentos'
+  },
+  {
+    id: 22,
+    nombre: 'Vinegre de alcohol',
+    marca: 'Menoyo',
+    precio: '$ 190',
+    categoria: 'Condimentos'
+  },
+];
